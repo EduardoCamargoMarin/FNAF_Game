f3089217fd3e939b5c283940e12a3bb38224e9bd
Eduardo Marin <108038506+EduardoCamargoMarin@users.noreply.github.com>
2023-06-15 21:18:13 -0300

    Add files via upload

--- FNAF Game/src/Main.java
+++ FNAF Game/src/Main.java
@@ -20,29 +20,232 @@ public class Main {
 
         //Salas
         Room escolhaSala = new Room();
-        escolhaSala.cozinha = Math.floor(Math.random() * 10);
-        escolhaSala.armario = Math.floor(Math.random() * 10);
-        escolhaSala.banheiro = Math.floor(Math.random() * 10);
-        escolhaSala.manutencao = Math.floor(Math.random() * 10);
-        escolhaSala.ventilacao = Math.floor(Math.random() * 10);
-        escolhaSala.camarote = Math.floor(Math.random() * 10);
-        escolhaSala.seguranca = Math.floor(Math.random() * 10);
+        escolhaSala.cozinha = 7;
+        escolhaSala.armario = 3;
+        escolhaSala.banheiro = 9;
+        escolhaSala.manutencao = 2;
+        escolhaSala.ventilacao = 1;
+        escolhaSala.camarote = 4;
+        escolhaSala.seguranca = 3;
 
-        if (playerChoice == 1) {
-            System.out.println("VOCE SE ESCONDEU NA COZINHA");
-            if (escolhaSala.cozinha != attackFreedy.actionFreddy) {
+
+            if (playerChoice == 1 && escolhaSala.cozinha != attackFreedy.actionFreddy) {
+                System.out.println("VOCÊ SE ESCONDEU NA COZINHA");
                 System.out.println("Freddy não te encontrou");
             }
-            if (escolhaSala.cozinha != attackChica.actionChica) {
+            if (playerChoice == 1 && escolhaSala.cozinha != attackChica.actionChica) {
                 System.out.println("Chica não te encontrou");
             }
-            if (escolhaSala.cozinha != attackFoxy.actionFoxy) {
-                System.out.println("Foxy não tem encontrou");
+            if (playerChoice == 1 && escolhaSala.cozinha != attackFoxy.actionFoxy) {
+                System.out.println("Foxy não te encontrou");
             }
-            if (escolhaSala.cozinha != attackBonnie.actionBonnie){
+            if (playerChoice == 1 && escolhaSala.cozinha != attackBonnie.actionBonnie){
                 System.out.println("Bonnie não te encontrou");
             }
+            //Caso te encontrem
+            if (playerChoice == 1 && escolhaSala.cozinha == attackFreedy.actionFreddy) {
+                System.out.println("Freddy te encontrou");
+                System.out.println("FIM DE JOGO");
+            }
+            if (playerChoice == 1 && escolhaSala.cozinha == attackChica.actionChica) {
+                System.out.println("Chica te encontrou");
+                System.out.println("FIM DE JOGO");
+            }
+            if (playerChoice == 1 && escolhaSala.cozinha == attackFoxy.actionFoxy) {
+                System.out.println("Foxy te encontrou");
+                System.out.println("FIM DE JOGO");
+            }
+            if (playerChoice == 1 && escolhaSala.cozinha == attackBonnie.actionBonnie){
+                System.out.println("Bonnie te encontrou");
+                System.out.println("FIM DE JOGO");
+            }
+
+        if (playerChoice == 2 && escolhaSala.cozinha != attackFreedy.actionFreddy) {
+            System.out.println("VOCÊ SE ESCONDEU NA SALA DO ZELADOR");
+            System.out.println("Freddy não te encontrou");
         }
+        if (playerChoice == 2 && escolhaSala.cozinha != attackChica.actionChica) {
+            System.out.println("Chica não te encontrou");
+        }
+        if (playerChoice == 2 && escolhaSala.cozinha != attackFoxy.actionFoxy) {
+            System.out.println("Foxy não te encontrou");
+        }
+        if (playerChoice == 2 && escolhaSala.cozinha != attackBonnie.actionBonnie){
+            System.out.println("Bonnie não te encontrou");
+        }
+        //Caso te encontrem
+        if (playerChoice == 2 && escolhaSala.cozinha == attackFreedy.actionFreddy) {
+            System.out.println("Freddy te encontrou");
+            System.out.println("FIM DE JOGO");
+        }
+        if (playerChoice == 2 && escolhaSala.cozinha == attackChica.actionChica) {
+            System.out.println("Chica te encontrou");
+            System.out.println("FIM DE JOGO");
+        }
+        if (playerChoice == 2 && escolhaSala.cozinha == attackFoxy.actionFoxy) {
+            System.out.println("Foxy te encontrou");
+            System.out.println("FIM DE JOGO");
+        }
+        if (playerChoice == 2 && escolhaSala.cozinha == attackBonnie.actionBonnie){
+            System.out.println("Bonnie te encontrou");
+            System.out.println("FIM DE JOGO");
+        }
+
+        if (playerChoice == 3 && escolhaSala.cozinha != attackFreedy.actionFreddy) {
+            System.out.println("VOCÊ SE ESCONDEU NO BANHEIRO");
+            System.out.println("Freddy não te encontrou");
+        }
+        if (playerChoice == 3 && escolhaSala.cozinha != attackChica.actionChica) {
+            System.out.println("Chica não te encontrou");
+        }
+        if (playerChoice == 3 && escolhaSala.cozinha != attackFoxy.actionFoxy) {
+            System.out.println("Foxy não te encontrou");
+        }
+        if (playerChoice == 3 && escolhaSala.cozinha != attackBonnie.actionBonnie){
+            System.out.println("Bonnie não te encontrou");
+        }
+        //Caso te encontrem
+        if (playerChoice == 3 && escolhaSala.cozinha == attackFreedy.actionFreddy) {
+            System.out.println("Freddy te encontrou");
+            System.out.println("FIM DE JOGO");
+        }
+        if (playerChoice == 3 && escolhaSala.cozinha == attackChica.actionChica) {
+            System.out.println("Chica te encontrou");
+            System.out.println("FIM DE JOGO");
+        }
+        if (playerChoice == 3 && escolhaSala.cozinha == attackFoxy.actionFoxy) {
+            System.out.println("Foxy te encontrou");
+            System.out.println("FIM DE JOGO");
+        }
+        if (playerChoice == 3 && escolhaSala.cozinha == attackBonnie.actionBonnie){
+            System.out.println("Bonnie te encontrou");
+            System.out.println("FIM DE JOGO");
+        }
+
+        if (playerChoice == 4 && escolhaSala.cozinha != attackFreedy.actionFreddy) {
+            System.out.println("VOCÊ SE ESCONDEU NA SALA DE MANUTENÇÃO");
+            System.out.println("Freddy não te encontrou");
+        }
+        if (playerChoice == 4 && escolhaSala.cozinha != attackChica.actionChica) {
+            System.out.println("Chica não te encontrou");
+        }
+        if (playerChoice == 4 && escolhaSala.cozinha != attackFoxy.actionFoxy) {
+            System.out.println("Foxy não te encontrou");
+        }
+        if (playerChoice == 4 && escolhaSala.cozinha != attackBonnie.actionBonnie){
+            System.out.println("Bonnie não te encontrou");
+        }
+        //Caso te encontrem
+        if (playerChoice == 4 && escolhaSala.cozinha == attackFreedy.actionFreddy) {
+            System.out.println("Freddy te encontrou");
+            System.out.println("FIM DE JOGO");
+        }
+        if (playerChoice == 4 && escolhaSala.cozinha == attackChica.actionChica) {
+            System.out.println("Chica te encontrou");
+            System.out.println("FIM DE JOGO");
+        }
+        if (playerChoice == 4 && escolhaSala.cozinha == attackFoxy.actionFoxy) {
+            System.out.println("Foxy te encontrou");
+            System.out.println("FIM DE JOGO");
+        }
+        if (playerChoice == 4 && escolhaSala.cozinha == attackBonnie.actionBonnie){
+            System.out.println("Bonnie te encontrou");
+            System.out.println("FIM DE JOGO");
+        }
+
+        if (playerChoice == 5 && escolhaSala.cozinha != attackFreedy.actionFreddy) {
+            System.out.println("VOCÊ SE ESCONDEU NA VENTILAÇÃO");
+            System.out.println("Freddy não te encontrou");
+        }
+        if (playerChoice == 5 && escolhaSala.cozinha != attackChica.actionChica) {
+            System.out.println("Chica não te encontrou");
+        }
+        if (playerChoice == 5 && escolhaSala.cozinha != attackFoxy.actionFoxy) {
+            System.out.println("Foxy não te encontrou");
+        }
+        if (playerChoice == 5 && escolhaSala.cozinha != attackBonnie.actionBonnie){
+            System.out.println("Bonnie não te encontrou");
+        }
+        //Caso te encontrem
+        if (playerChoice == 5 && escolhaSala.cozinha == attackFreedy.actionFreddy) {
+            System.out.println("Freddy te encontrou");
+            System.out.println("FIM DE JOGO");
+        }
+        if (playerChoice == 5 && escolhaSala.cozinha == attackChica.actionChica) {
+            System.out.println("Chica te encontrou");
+            System.out.println("FIM DE JOGO");
+        }
+        if (playerChoice == 5 && escolhaSala.cozinha == attackFoxy.actionFoxy) {
+            System.out.println("Foxy te encontrou");
+            System.out.println("FIM DE JOGO");
+        }
+        if (playerChoice == 5 && escolhaSala.cozinha == attackBonnie.actionBonnie){
+            System.out.println("Bonnie te encontrou");
+            System.out.println("FIM DE JOGO");
+        }
+
+        if (playerChoice == 6 && escolhaSala.cozinha != attackFreedy.actionFreddy) {
+            System.out.println("VOCÊ SE ESCONDEU NO CAMAROTE");
+            System.out.println("Freddy não te encontrou");
+        }
+        if (playerChoice == 6 && escolhaSala.cozinha != attackChica.actionChica) {
+            System.out.println("Chica não te encontrou");
+        }
+        if (playerChoice == 6 && escolhaSala.cozinha != attackFoxy.actionFoxy) {
+            System.out.println("Foxy não te encontrou");
+        }
+        if (playerChoice == 6 && escolhaSala.cozinha != attackBonnie.actionBonnie){
+            System.out.println("Bonnie não te encontrou");
+        }
+        //Caso te encontrem
+        if (playerChoice == 6 && escolhaSala.cozinha == attackFreedy.actionFreddy) {
+            System.out.println("Freddy te encontrou");
+            System.out.println("FIM DE JOGO");
+        }
+        if (playerChoice == 6 && escolhaSala.cozinha == attackChica.actionChica) {
+            System.out.println("Chica te encontrou");
+            System.out.println("FIM DE JOGO");
+        }
+        if (playerChoice == 6 && escolhaSala.cozinha == attackFoxy.actionFoxy) {
+            System.out.println("Foxy te encontrou");
+            System.out.println("FIM DE JOGO");
+        }
+        if (playerChoice == 6 && escolhaSala.cozinha == attackBonnie.actionBonnie){
+            System.out.println("Bonnie te encontrou");
+            System.out.println("FIM DE JOGO");
+        }
+
+        if (playerChoice == 7 && escolhaSala.cozinha != attackFreedy.actionFreddy) {
+            System.out.println("VOCÊ SE ESCONDEU NA SALA DE SEGURANÇA");
+            System.out.println("Freddy não te encontrou");
+        }
+        if (playerChoice == 7 && escolhaSala.cozinha != attackChica.actionChica) {
+            System.out.println("Chica não te encontrou");
+        }
+        if (playerChoice == 7 && escolhaSala.cozinha != attackFoxy.actionFoxy) {
+            System.out.println("Foxy não te encontrou");
+        }
+        if (playerChoice == 7 && escolhaSala.cozinha != attackBonnie.actionBonnie){
+            System.out.println("Bonnie não te encontrou");
+        }
+        //Caso te encontrem
+        if (playerChoice == 7 && escolhaSala.cozinha == attackFreedy.actionFreddy) {
+            System.out.println("Freddy te encontrou");
+            System.out.println("FIM DE JOGO");
+        }
+        if (playerChoice == 7 && escolhaSala.cozinha == attackChica.actionChica) {
+            System.out.println("Chica te encontrou");
+            System.out.println("FIM DE JOGO");
+        }
+        if (playerChoice == 7 && escolhaSala.cozinha == attackFoxy.actionFoxy) {
+            System.out.println("Foxy te encontrou");
+            System.out.println("FIM DE JOGO");
+        }
+        if (playerChoice == 7 && escolhaSala.cozinha == attackBonnie.actionBonnie){
+            System.out.println("Bonnie te encontrou");
+            System.out.println("FIM DE JOGO");
+        }
+
     }}
 
 //NÃO ESTA PRONTO, PRECISA FINALIZAR!
